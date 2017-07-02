9ed9af200fe7a561487fa989e77e1f2b7a7ef550
Unit111 <unit_1@abv.bg>
2017-07-02 12:53:19 +0300

    First commit

---
+++ piche-core/src/main/java/TestClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Unit 1 on 02-Jul-17.
+ */
+public class TestClass {
+
+    public String returnString() {
+        return "works!";
+    }
+}
---
+++ piche-tests/src/test/java/StepDefs.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Unit 1 on 02-Jul-17.
+ */
+public class StepDefs {
+
+    TestClass testClass;
+
+
+}
